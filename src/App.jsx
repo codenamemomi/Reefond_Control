ce64985cca4c26ec77deabd0e8562ef75b1c2b66
Omomijolaoluwa <akinrogundecodenamemomi@gmail.com>
2026-02-25 20:23:25 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="base-container">
+      <h1>Reefond Control</h1>
+      <p>Base form initialized. Waiting for next instruction.</p>
+    </div>
+  );
+}
+
+export default App;
